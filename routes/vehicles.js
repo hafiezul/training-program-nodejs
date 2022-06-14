246a598e85ac93793dad6aefd26997df270890a7
HJ88956 <hafiezul.zikry-johari@edag.com.my>
2022-06-14 16:53:35 +0800

    Vehicle routes & services

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -18,4 +18,53 @@ router.get("/", async function (req, res, next) {
   }
 });
 
+/*
+ * GET /api/vehicles/user/:id
+ * Get all vehicles by userId
+ * @param {Number} id
+ * @param {Number} page
+ * @returns {Array}
+ * @throws {Error}
+ */
+router.get("/user/:id", async function (req, res, next) {
+  try {
+    res.json(await vehicles.getByUserId(req.params.id, req.query.page));
+  } catch (err) {
+    console.error(`Error : `, err.message);
+    next(err);
+  }
+});
+
+/*
+ * GET /api/vehicles/:id
+ * Get a vehicle by id
+ * @returns {Object}
+ * @throws {Error}
+ * @param {Number} id
+ */
+router.get("/:id", async function (req, res, next) {
+  try {
+    res.json(await vehicles.getById(req.params.id));
+  } catch (err) {
+    console.error(`Error : `, err.message);
+    next(err);
+  }
+});
+
+/*
+ * POST /api/vehicles
+ * Create a new vehicle
+ * @returns {Object}
+ * @throws {Error}
+ * @body {Object}
+ */
+router.post("/", async function (req, res, next) {
+  try {
+    res.json(await vehicles.create(req.body));
+  } catch (err) {
+    console.error(`Error : `, err.message);
+    next(err);
+  }
+});
+
 module.exports = router;
--- services/vehicles.js
+++ services/vehicles.js
@@ -6,15 +6,56 @@ async function getMultiple(page = 1) {
     page: page,
     withRelated: ["user"],
   });
-  // return posts;
+
+  return {
+    totalPages: posts.pagination.pageCount,
+    currentPage: posts.pagination.page,
+    data: posts,
+  };
+}
+
+async function create(data) {
+  try {
+    const vehicle = await Vehicle.forge(data).save();
+
+    if (vehicle) {
+      return {
+        status: 200,
+        message: "Vehicle created successfully",
+        data: vehicle,
+      };
+    } else {
+      return {
+        status: 404,
+        message: "Vehicle not created",
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      message: error.message,
+      data: null,
+    };
+  }
+}
+
+async function getByUserId(userId, page = 1) {
+  const vehicles = await Vehicle.where("UserID", userId).fetchPage({
+    pageSize: 5,
+    page: page,
+    withRelated: ["user"],
+  });
 
   return {
     status: 200,
-    message: "Posts fetched successfully",
-    data: posts,
+    message: "Vehicles fetched successfully",
+    data: vehicles,
   };
 }
 
 module.exports = {
   getMultiple,
+  create,
+  getByUserId,
 };
